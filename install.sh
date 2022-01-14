ff266c42bf731f7e93d49953b45853f0a2460eec
GitKeu <93443640+GitKeu@users.noreply.github.com>
2022-01-14 19:01:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-
+clear
+echo "RONIKEURY domina"
+sleep 3
 apt-get update
 apt-get upgrade
 apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install
 git clone https://github.com/GitKeu/nm
 echo "SEJA PACIENTE, ESPERE 120 SEGUNDOS"
 sleep 50
